8c1efb158bfdd6c3cba45f876b4b1466fbfd26d8
David Newton <david@davidjnewton.com>
2022-10-06 12:58:40 +0100

    ps role

--- site-modules/profile/manifests/dashboard.pp
+++ site-modules/profile/manifests/dashboard.pp
@@ -1,14 +1,14 @@
 # operational dashboard
 class profile::dashboard {
-  include puppet_operational_dashboards::enterprise_infrastructure
-  include puppet_operational_dashboards
+  if $facts['toml_gem_installed'] {
+    include puppet_operational_dashboards::enterprise_infrastructure
+    include puppet_operational_dashboards
+  } else {
+    warning('toml-rb gem is not installed - not managing puppet_operational_dashboards')
+  }
   package { 'toml-rb':
     ensure   => 'present',
     provider => 'puppetserver_gem',
-    before   => [
-      Class['puppet_operational_dashboards'],
-      Class['puppet_operational_dashboards::enterprise_infrastructure'],
-    ],
-    notify => Service['pe-puppetserver'],
+    notify   => Service['pe-puppetserver'],
   }
 }

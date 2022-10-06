54657a9858aaaa4b99aa152e5b5fa835940f4b03
David Newton <david@davidjnewton.com>
2022-10-06 12:50:28 +0100

    ps role

--- site-modules/profile/manifests/dashboard.pp
+++ site-modules/profile/manifests/dashboard.pp
@@ -2,16 +2,13 @@
 class profile::dashboard {
   include puppet_operational_dashboards::enterprise_infrastructure
   include puppet_operational_dashboards
-  ensure_packages(
-    ['toml-rb'],
-    { ensure   => 'present',
-      provider => 'puppetserver_gem',
-      before   => [
-        Class['puppet_operational_dashboards'],
-        Class['puppet_operational_dashboards::enterprise_infrastructure']
-      ],
-      notify => Service['pe-puppetserver'],
-      
-    }
-  )
+  package { 'toml-rb':
+    ensure   => 'present',
+    provider => 'puppetserver_gem',
+    before   => [
+      Class['puppet_operational_dashboards'],
+      Class['puppet_operational_dashboards::enterprise_infrastructure'],
+    ],
+    notify => Service['pe-puppetserver'],
+  }
 }

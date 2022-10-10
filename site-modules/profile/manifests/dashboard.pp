5415b3ad6db78bbf1b455b9d1d018a89288ede61
David Newton <david@davidjnewton.com>
2022-10-10 20:36:07 +0100

    influxdb host for telegraf

--- site-modules/profile/manifests/dashboard.pp
+++ site-modules/profile/manifests/dashboard.pp
@@ -2,13 +2,20 @@
 class profile::dashboard {
   if $facts['toml-rb_installed'] {
     include puppet_operational_dashboards::enterprise_infrastructure
-    include puppet_operational_dashboards
     class { 'puppet_operational_dashboards::telegraf::agent':
-      influxdb_host => 'puppetserver1.lab.local',
-      token         => Sensitive('KKpcL9OER9UwBnitvBK38hMWRp7aKp9VzkZHSvzovqmXGBBXJcBRXTekskCFL1n-DwJKNJQnI8vmp1R7L6WWwA=='),
-      require       => Class['puppet_operational_dashboards'],
+      influxdb_host       => 'puppetserver1.lab.local',
+      token               => Sensitive('KKpcL9OER9UwBnitvBK38hMWRp7aKp9VzkZHSvzovqmXGBBXJcBRXTekskCFL1n-DwJKNJQnI8vmp1R7L6WWwA=='),
+      token_name          => 'puppet telegraf token',
+      influxdb_token_file => lookup(influxdb::token_file, undef, undef, $facts['identity']['user'] ? {
+        'root'  => '/root/.influxdb_token',
+        default => "/home/${facts['identity']['user']}/.influxdb_token"
+      }),
+      influxdb_port       => lookup(influxdb::port, undef, undef, 8086),
+      initial_org         => lookup(influxdb::initial_org, undef, undef, 'puppetlabs'),
+      initial_bucket      => lookup(influxdb::initial_bucket, undef, undef, 'puppet_data'),
+      use_ssl             => true,
     }
-x
+
   } else {
     warning('toml-rb gem is not installed - not managing puppet_operational_dashboards')
     package { 'toml-rb':

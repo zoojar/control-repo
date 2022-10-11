cc651e7b911a7694e8bb4521a030009620d6730a
David Newton <david@davidjnewton.com>
2022-10-11 16:59:37 +0100

    moved to data

--- site-modules/profile/manifests/dashboard.pp
+++ site-modules/profile/manifests/dashboard.pp
@@ -1,7 +1,7 @@
 # operational dashboard
 class profile::dashboard (
   Optional[Sensitive[String]] $influxdb_token,
-  Strong $influxdb_token_name,
+  String $influxdb_token_name,
   String $influxdb_host,
   Integer $influxdb_port,
   String $influxdb_org,

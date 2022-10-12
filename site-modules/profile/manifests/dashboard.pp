980f6aade5ddfa5a1388f3b3d197b64644e116a1
David Newton <david@davidjnewton.com>
2022-10-12 21:03:27 +0100

    data

--- site-modules/profile/manifests/dashboard.pp
+++ site-modules/profile/manifests/dashboard.pp
@@ -10,15 +10,16 @@ class profile::dashboard {
       provider => 'puppetserver_gem',
       notify   => Service['pe-puppetserver'],
     }
+    file { '/opt/puppetlabs/facter/facts.d/toml-rb_installed.sh':
+      ensure  => file,
+      mode    => '0500',
+      content => @(EOS)
+        #!/bin/bash
+        echo toml-rb_installed=`/opt/puppetlabs/bin/puppetserver gem list toml-rb -i`
+        | EOS
+    }
   }
-  file { '/opt/puppetlabs/facter/facts.d/toml-rb_installed.sh':
-    ensure  => file,
-    mode    => '0500',
-    content => @(EOS)
-      #!/bin/bash
-      echo toml-rb_installed=`/opt/puppetlabs/bin/puppetserver gem list toml-rb -i`
-      | EOS
-  }
+
 }
 
 

3fb70c162ed06137b34517ca63475a97885f443e
David Newton <david@davidjnewton.com>
2022-10-12 14:59:46 +0100

    move to hiera

--- site-modules/profile/manifests/dashboard.pp
+++ site-modules/profile/manifests/dashboard.pp
@@ -1,7 +1,24 @@
 # operational dashboard
 class profile::dashboard {
-  include puppet_operational_dashboards::enterprise_infrastructure
-  include puppet_operational_dashboards
+   if $facts['toml-rb_installed'] {
+    include puppet_operational_dashboards::enterprise_infrastructure
+    include puppet_operational_dashboards
+  } else {
+    warning('toml-rb gem is not installed - not managing puppet_operational_dashboards')
+    package { 'toml-rb':
+      ensure   => 'present',
+      provider => 'puppetserver_gem',
+      notify   => Service['pe-puppetserver'],
+    }
+  }
+  file { '/opt/puppetlabs/facter/facts.d/toml-rb_installed.sh':
+    ensure  => file,
+    mode    => '0500',
+    content => @(EOS)
+      #!/bin/bash
+      echo toml-rb_installed=`/opt/puppetlabs/bin/puppetserver gem list toml-rb -i`
+      | EOS
+  }
 }
 
 

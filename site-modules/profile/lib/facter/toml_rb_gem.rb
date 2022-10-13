d34d4d8a78a4e214348281842589fbb5822e12eb
David Newton <david@davidjnewton.com>
2022-10-13 21:18:30 +0100

    toml gem version

--- site-modules/profile/lib/facter/toml_rb_gem.rb
+++
@@ -1,7 +0,0 @@
-Facter.add(:toml_rb_gem_status) do
-  confine kernel: 'Linux'
-  cmd = '/opt/puppetlabs/bin/puppetserver gem list toml-rb -i'
-  setcode do
-    { gem_installed: Facter::Core::Execution.execute(cmd).tr('\"', '') }
-  end
-end
---
+++ site-modules/profile/lib/facter/toml_rb_gem_status.rb
@@ -0,0 +1,7 @@
+Facter.add(:toml_rb_gem_status) do
+  confine kernel: 'Linux'
+
+  setcode do
+    { gem_installed: Facter::Core::Execution.execute('/opt/puppetlabs/bin/puppetserver gem list toml-rb -i').to_boolean }
+  end
+end

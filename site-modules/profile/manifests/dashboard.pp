c9ae4f825932c69476b3f7b6c7072a39902a0c29
David Newton <david@davidjnewton.com>
2022-10-13 15:38:08 +0100

    facts

--- site-modules/profile/manifests/dashboard.pp
+++ site-modules/profile/manifests/dashboard.pp
@@ -4,10 +4,10 @@ class profile::dashboard (
 ) {
   if $facts['toml-rb_installed'] {
     include puppet_operational_dashboards::enterprise_infrastructure
-    unless $agent_only {
-      include puppet_operational_dashboards
-    } else {
+    if $agent_only {
       include 'puppet_operational_dashboards::telegraf::agent'
+    } else {
+      include puppet_operational_dashboardss
     }
   } else {
     warning('toml-rb gem is not installed - not managing puppet_operational_dashboards')
@@ -16,18 +16,5 @@ class profile::dashboard (
       provider => 'puppetserver_gem',
       notify   => Service['pe-puppetserver'],
     }
-    file { '/opt/puppetlabs/facter/facts.d/toml-rb_installed.sh':
-      ensure  => file,
-      mode    => '0500',
-      content => @(EOS)
-        #!/bin/bash
-        echo toml-rb_installed=`/opt/puppetlabs/bin/puppetserver gem list toml-rb -i`
-        | EOS
-    }
   }
-
 }
-
-
-
-

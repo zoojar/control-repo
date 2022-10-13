0432e891a891c21a1427a9abd10ab2b8f5714d7b
David Newton <david@davidjnewton.com>
2022-10-13 15:51:51 +0100

    rm c fact

--- site-modules/profile/lib/facter/toml-rb_installed.rb
+++
@@ -1,6 +0,0 @@
-Facter.add('toml-rb_installed') do
-  confine kernel: 'Linux'
-  setcode do
-    Facter::Core::Execution.execute('/opt/puppetlabs/bin/puppetserver gem list toml-rb -i')
-  end
-end
--- site-modules/profile/manifests/dashboard.pp
+++ site-modules/profile/manifests/dashboard.pp
@@ -2,12 +2,12 @@
 class profile::dashboard (
   Boolean $agent_only = true,
 ) {
-  if $facts['toml-rb_installed'] {
+  if $facts['toml_rb_gem']['gem_installed'] {
     include puppet_operational_dashboards::enterprise_infrastructure
     if $agent_only {
       include 'puppet_operational_dashboards::telegraf::agent'
     } else {
-      include puppet_operational_dashboardss
+      include puppet_operational_dashboards
     }
   } else {
     warning('toml-rb gem is not installed - not managing puppet_operational_dashboards')

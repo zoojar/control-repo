f651ca9675a96e027c1cd7424e17f6ce94880719
zoojar <david@davidjnewton.com>
2022-11-07 16:38:04 +0000

    dashboard toml order

--- site-modules/role/manifests/dashboard.pp
+++ site-modules/role/manifests/dashboard.pp
@@ -1,10 +1,4 @@
 # puppetserver
 class role::dashboard {
   include profile::base
-  include profile::dashboard_agent
-  include profile::dashboard
-
-  Class['profile::base']
-  ->Class['profile::dashboard_agent']
-  ->Class['profile::dashboard']
-}
+  }

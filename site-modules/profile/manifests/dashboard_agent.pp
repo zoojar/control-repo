620e7bbaea2dc184685c962c415cd5c7722de8d6
zoojar <david@davidjnewton.com>
2022-11-09 14:10:16 +0000

    keyyy

--- site-modules/profile/manifests/dashboard_agent.pp
+++ site-modules/profile/manifests/dashboard_agent.pp
@@ -4,12 +4,10 @@ class profile::dashboard_agent (
   String $telegraf_agent_token_file = '/root/.telegraf_agent_token',
   Optional[Sensitive[String]] $telegraf_agent_token = undef,
 ) {
-  $_telegraf_agent_token = $telegraf_agent_token ? {
-    undef   => Deferred('find_file', [$telegraf_agent_token_file]) ? {
-      /\/\S+/ => Sensitive(Deferred('file', [$telegraf_agent_token_file])),
-      default => '',
-    },
-    default => $telegraf_agent_token,
+  unless ($telegraf_agent_token != undef) or (Deferred('find_file', [$telegraf_agent_token_file]) == undef) {
+    $_telegraf_agent_token = Sensitive(Deferred('file', [$telegraf_agent_token_file]))
+  } else {
+    $_telegraf_agent_token = ''
   }
   # puppet_operational_dashboards module uses the to_toml() function,
   # which depends on the toml-rb gem,

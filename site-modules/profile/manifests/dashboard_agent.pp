bb4a83826fdda54859070381289f5b5a2cd89bd3
zoojar <david@davidjnewton.com>
2022-11-10 14:06:57 +0000

    token rm

--- site-modules/profile/manifests/dashboard_agent.pp
+++ site-modules/profile/manifests/dashboard_agent.pp
@@ -10,7 +10,7 @@ class profile::dashboard_agent (
     include puppet_operational_dashboards::enterprise_infrastructure
     class { 'puppet_operational_dashboards::telegraf::agent':
       token_name          => 'puppet telegraf token',
-      token               => undef, # $telegraf_agent_token,
+      token               => $telegraf_agent_token,
       influxdb_token_file => '/root/.influxdb_token',
       influxdb_host       => $influxdb_host,
       influxdb_port       => 8086,

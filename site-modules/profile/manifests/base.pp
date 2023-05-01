7156af70d7a236b7d3e11ee1b6f0c9dbd3daa4c3
zoojar <david@davidjnewton.com>
2023-05-01 20:05:58 +0100

    role base

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,7 +1,7 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
   class {'::puppet_agent':
-    package_version => '6.28.0',
-    collection      => 'puppet6',
+    package_version => '7.21.0',
+    collection      => 'puppet7',
   }
 }

b9ed560bb139c625679df26c9df4cdb4d9b0d729
zoojar <david@davidjnewton.com>
2023-06-08 10:34:33 +0100

    fm

--- site-modules/role/manifests/primary.pp
+++ site-modules/role/manifests/primary.pp
@@ -1,3 +1,11 @@
 # puppetserver
-class role::puppetserver {
+class role::primary {
+  include base
+  include puppet::server::puppetdb   # for server config
+  include r10k
+  include ssh
+  include foreman::repo
+  include foreman_proxy
+  Group['puppet'] -> Class['foreman_proxy']  # issue in foreman_proxy module: https://github.com/theforeman/puppet-foreman_proxy/issues/295
+  Class['foreman::repo'] -> Class['foreman_proxy']
 }
